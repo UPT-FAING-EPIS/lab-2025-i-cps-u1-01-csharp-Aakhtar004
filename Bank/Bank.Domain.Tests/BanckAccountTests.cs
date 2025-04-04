b8d3d255e202ed5d50e1127beba6fd1182f3d676
Ahmed <aa2022074261@virtual.upt.pe>
2025-04-03 21:15:58 -0500

    Implementacion de segundo test

--- Bank/Bank.Domain.Tests/BanckAccountTests.cs
+++ Bank/Bank.Domain.Tests/BanckAccountTests.cs
@@ -19,5 +19,21 @@ namespace Bank.Domain.Tests
             double actual = account.Balance;
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
+         [Test]
+        public void Credit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double creditAmount = 5.00;
+            double expected = 16.99;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            
+            // Act
+            account.Credit(creditAmount);
+            
+            // Assert
+            double actual = account.Balance;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, actual, 0.001, "Account not credited correctly");
+        }
     }
 }
